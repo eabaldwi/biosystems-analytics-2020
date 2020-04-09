4d66d943d5f41a46b260a0974f5c9e18103554b2
Edwin Baldwin <edwin@MacBook-Pro-5.local>
2020-04-09 10:34:14 -0700

    should be 100% if i can get Bio working?

--- assignments/09_fa_sampler/sampler.py
+++ assignments/09_fa_sampler/sampler.py
@@ -8,9 +8,13 @@ Purpose: Rock the Casbah
 import argparse
 import os
 import sys
-#import Bio
-#from Bio import Seq
-#from Bio import SeqIO
+import random
+import Bio
+from Bio import Seq
+from Bio import SeqIO
+# import biopython
+# from biopython import Seq
+# from biopython import SeqIO
 
 
 
@@ -25,7 +29,7 @@ def get_args():
     parser.add_argument('file',
                         metavar='FILE',
                         nargs='+',
-                        help='Readable file(s)',
+                        help='Input FASTA file(s)',
                         type=argparse.FileType('r'))
 
     parser.add_argument('-p',
@@ -63,7 +67,37 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    print(args)
+    random.seed(args.seed)
+
+    out_dir = args.outdir
+
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    num_seqs = 0
+    num_files = 0
+
+    for fh in args.file:
+        num_files +=1
+        basename = os.path.basename(fh.name)
+        out_file = os.path.join(args.outdir, basename)
+        print(f'{num_files:3}: {basename}')
+        out_fh = open(out_file, 'wt')
+
+        for rec in SeqIO.parse(fh, 'fasta'):
+            if random.random() <= args.pct:
+                SeqIO.write(rec, out_fh, 'fasta')
+                sequences += 1
+            num_seqs += sequences
+
+        out_fh.close()
+
+    if num_seqs > 1 and num_files > 1:
+        print(f'Wrote {num_seqs} sequences in {num_files} files to directory "{args.outdir}"')
+    elif sequences > 1 and files == 1:
+        print(f'Wrote {num_seqs} sequences in {num_files} file to directory "{args.outdir}"')
+    else:
+        print(f'Wrote {num_seqs} sequence in {num_files} file to directory "{args.outdir}"')
 
 
 # --------------------------------------------------
