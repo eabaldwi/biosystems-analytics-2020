ecd0b54b20168e0f71d7a3d0699217aa27bc7215
Edwin Baldwin <edwin@MacBook-Pro-5.local>
2020-04-15 22:08:05 -0700

    added start hw 10

---
+++ assignments/10_unclustered_proteins/find_unclustered.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Author : edwin
+Date   : 2020-04-15
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-p',
+                        '--proteins',
+                        metavar='proteins',
+                        help='Proteins FASTA',
+                        type=argparse.FileType('r'))
+
+    parser.add_argument('-c',
+                        '--cdhit',
+                        help='Output file from CD-HIT',
+                        metavar='cdhit',
+                        type=argparse.FileType('rw'),
+                        default=None)
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='output file',
+                        metavar='outFILE',
+                        type=argparse.FileType('wt'),
+                        default='unclustered.fa')
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    print(args)
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
