5d5b415cda003e29e39fc37e8d6ebfc57d37e613
Edwin Baldwin <edwin@MacBook-Pro-5.local>
2020-04-30 10:55:50 -0700

    added swisstake with test edited

--- assignments/11_swisstake/swisstake.py
+++ assignments/11_swisstake/swisstake.py
@@ -31,6 +31,7 @@ def get_args():
                         help='Keyword to take',
                         metavar='str',
                         type=str,
+                        nargs="+",
                         default=None,
                         required=True)
 
@@ -57,32 +58,64 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
+   # wanted_kw = set([kw.lower() for kw in args.keyword])
+    wanted_kw = set(map(str.lower, args.keyword))
+    skip_taxa = set(map(str.lower, args.skiptaxa or []))
+    num_taken, num_skipped = 0,0
 
-    recskipped = 0
+    for rec in SeqIO.parse(args.file, 'swiss'):
+        annots = rec.annotations
 
-    if args.skiptaxa:
-        skip = set(map(str.lower, args.skiptaxa))
+        taxa = annots.get('taxonomy')
+        if taxa:
+            taxa = set(map(str.lower, taxa))
+            if skip_taxa.intersection(taxa):
+                num_skipped +=1
+                continue
 
-    totaltake = 0
+        keywords = annots.get('keywords')
+        if keywords:
+            keywords = set(map(str.lower, keywords))
+            if wanted_kw.intersection(keywords):
+                num_taken +=1
+                SeqIO.write(rec, args.outfile, 'fasta')
+            else:
+                num_skipped += 1
 
-    for rec in SeqIO.parse(args.file, "swiss"):
-        if 'taxonomy' in rec.annotations:
-            taxonomy = rec.annotations["taxonomy"]
-            taxa = set(map(str.lower, taxonomy))
-            skipped = skip.intersection(taxa)
-            recskipped += len(skipped)
-            if skipped == True:
-                next(rec)
-        else:
-            if 'keywords' in rec.annotations:
-                keywords = rec.annotations["keywords"]
-                keys = set(map(str.lower, keywords))
-                totaltake += len(keys)
-                if keys == True:
-                  SeqIO.write(rec, args.outfile, 'fasta')
-                else: next()
 
-    print(f'Done, skipped {recskipped} and took {totaltake}. See output in "{args.outfile.name}".')
+    print(f'Done, skipped {num_skipped} and took {num_taken}. See output in "{args.outfile.name}".')
+
+
+
+    #print(keywords.intersection(set(annots.get('keywords'))))
+
+
+
+    # recskipped = 0
+    #
+    # if args.skiptaxa:
+    #     skip = set(map(str.lower, args.skiptaxa))
+    #
+    # totaltake = 0
+    #
+    # for rec in SeqIO.parse(args.file, "swiss"):
+    #     if 'taxonomy' in rec.annotations:
+    #         taxonomy = rec.annotations["taxonomy"]
+    #         taxa = set(map(str.lower, taxonomy))
+    #         skipped = skip.intersection(taxa)
+    #         recskipped += len(skipped)
+    #         if skipped == True:
+    #             next(rec)
+    #     else:
+    #         if 'keywords' in rec.annotations:
+    #             keywords = rec.annotations["keywords"]
+    #             keys = set(map(str.lower, keywords))
+    #             totaltake += len(keys)
+    #             if keys == True:
+    #               SeqIO.write(rec, args.outfile, 'fasta')
+    #             else: next()
+    #
+    # print(f'Done, skipped {recskipped} and took {totaltake}. See output in "{args.outfile.name}".')
 
 
 
